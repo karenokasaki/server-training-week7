c66e65119f76e3d666aea59c5dc3aa1ceeaa7a45
karen okasaki <oks.karen@gmail.com>
2022-11-27 22:20:07 -0300

    iteração 1

--- index.js
+++ index.js
@@ -14,29 +14,48 @@ const app = express();
 app.use(express.json());
 
 //banco de dados que usaremos de exemplo
-const db = [];
-/* 
-    "nome": "karen okasaki", 
-    "role": "professora",
-    "tarefas": [],
-    "endereço": {
-        "cidade": "ribas do rio pardo",
-        "estado": "MS"
-    }
-*/
+const db = [
+  {
+    id: "e27ab2b1-cb91-4b18-ab90-5895cc9abd29",
+    documentName: "Licitação Enap - Curso Web Dev",
+    status: "Em andamento",
+    details:
+      "Processo para capacitação de servidores públicos em desenvolvimento de aplicações na WEB. Parceria com Ironhack",
+    dateInit: "28/11/2022",
+    comments: [
+      "Processo aberto",
+      "Processo partiu para as partes assinarem",
+      "Processo agora está em análise final",
+      "Processo já tem data final",
+    ],
+    dateEnd: "16/12/2022",
+    setor: "enap",
+  },
+  {
+    id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173",
+    documentName: "Licitação Compras - Notebooks",
+    status: "Em andamento",
+    details: "Processo de licitação para compra de notebooks",
+    dateInit: "30/11/2022",
+    comments: ["Processo em aberto e sem previsão de conclusão"],
+    dateEnd: "",
+    setor: "tre",
+  },
+  {
+    id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173",
+    documentName: "Licitação Compras - Ar Condicionado",
+    status: "Finalizado",
+    details: "Processo de licitação para compra de ar-condicionado",
+    dateInit: "15/11/2022",
+    comments: ["Processo em aberto", "Processo finalizado"],
+    dateEnd: "25/11/2022",
+    setor: "trj",
+  },
+];
 
-//criação das rotas
-//verbo http get
-app.get("/", (req, res) => {
-  // rota no insomnia: http://localhost:8080/
+//ITERAÇÃO 1
 
-  //req é a REQUISIÇÃO -> é o pedido, vem do cliente
-  // pelo req a gente pode acessar algumas informações como:
-  console.log(req.url);
-  console.log(req.method);
-
-  // res é RESPONSE -> é o que o nosso servidor vai devolver para o cliente
-  // retornar a resposta com o status de 200 e o json com a mensagem a seguir
+app.get("/all", (req, res) => {
   return res
     .status(200)
     .json({ messagem: "Bem vindo ao servidor da turma 91 - ENAP", data: db });
